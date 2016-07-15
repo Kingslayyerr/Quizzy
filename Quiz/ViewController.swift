dd851e21d822593cd34ae2f0a7dad8f52999a984
Christian Espinoza <chrs_espi@yahoo.com>
2016-07-14 17:31:35 -0700

    Add more questions/Initial animations

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
     
     let questions: [String] = ["What is Christian's favortie movie?",
                                "What is Christian's favorite book?",
-                               "What is Christian's favorite hobby?"]
+                               "What is Christian's favorite hobby?",
+                               "How old is Christian?",
+                               "When is Christian's Birthday?",
+                               "What is his favorite restaurant?"]
     
     let answers: [String] = ["Zoolander",
                              "Think & Grow Rich",
-                             "Playing Soccer"]
+                             "Playing Soccer",
+                             "26",
+                             "January 11, 1990",
+                             "Boiling Crab"]
     
     var currentQuestionIndex: Int = 0
     
@@ -31,6 +37,10 @@ class ViewController: UIViewController {
         let question: String = questions[currentQuestionIndex]
         questionLabel.text = question
         answerLabel.text = "???"
+        
+        //HERE WE CALL THE CLOSURE WE DEFINED FOR ANIMATION WHEN THE USER CLICKS ON THE NEXT BUTTON
+        
+        animateLabelTransitions()
     }
     
     @IBAction func showAnswer (sender: AnyObject) {
@@ -44,6 +54,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    //OVERRIDE viewWillAppear to set the alpha to 0, this will make the animation visible.
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set labels initial alpha
+        
+        questionLabel.alpha = 0
+        
+        }
+    
+    
+    //ANIMATIONS FOR QUIZ LABELS.
+    
+    func animateLabelTransitions() {
+        
+       // let animationClosure = { () -> Void in
+            
+        //    self.questionLabel.alpha = 1
+            
+       // }
+        
+        //ANIMATE THE alpha
+        
+        UIView.animateWithDuration(0.5, animations: {
+            self.questionLabel.alpha = 1
+        })
+        
+        
     }
     
 
